d5f6c6a8d881bb58cf1a75ec09bae112e617e8a4
Wycer <wycers@gmail.com>
2020-12-15 05:03:24 +0800

    ✨ judger login

--- internal/taskManager/remote.go
+++ internal/taskManager/remote.go
@@ -1,11 +1,15 @@
 package taskManager
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"strings"
 
+	cookiejar "github.com/juju/persistent-cookiejar"
+
 	"github.com/go-resty/resty/v2"
+	"github.com/infinity-oj/server-v2/pkg/models"
 )
 
 type remoteTaskManager struct {
@@ -14,9 +18,7 @@ type remoteTaskManager struct {
 }
 
 func (tm *remoteTaskManager) Reserve(task *Task) error {
-	url := fmt.Sprintf("%s/api/v1/task/%s/reservation",
-		tm.baseUrl,
-		task.TaskId)
+	url := fmt.Sprintf("/task/%s/reservation", task.TaskId)
 
 	resp, err := tm.client.R().
 		EnableTrace().
@@ -39,7 +41,7 @@ func (tm *remoteTaskManager) Reserve(task *Task) error {
 }
 
 func (tm *remoteTaskManager) Fetch(tp string) (*Task, error) {
-	url := fmt.Sprintf("%s/api/v1/task", tm.baseUrl)
+	url := fmt.Sprintf("/task")
 
 	resp, err := tm.client.R().
 		EnableTrace().
@@ -80,35 +82,153 @@ func (tm *remoteTaskManager) Fetch(tp string) (*Task, error) {
 
 		Properties: properties,
 		Inputs:     strings.Split(tmp.Inputs, ","),
-		Outputs:    "",
+		Outputs:    [][]byte{},
 	}
 	return task, nil
 }
 
-func (tm *remoteTaskManager) Push(task *Task) error {
-	url := fmt.Sprintf("%s/api/v1/task/%s",
-		tm.baseUrl,
-		task.TaskId)
+func (tm *remoteTaskManager) CreateFile(volume, filename string, file []byte) error {
 
-	_, err := tm.client.R().
-		EnableTrace().
-		SetBody(struct {
-			Token   string `json:"token"`
-			Outputs string `json:"outputs"`
-		}{
-			task.token,
-			task.Outputs,
-		}).
-		Put(url)
+	url := fmt.Sprintf("/volume/%s/file", volume)
+	resp, err := tm.client.R().
+		SetFileReader(
+			"file", filename, bytes.NewReader(file)).
+		Post(url)
+
 	if err != nil {
 		return err
 	}
+
+	// Explore response object
+	fmt.Println("Response Info:")
+	fmt.Println("  ", resp.Request.URL)
+	fmt.Println("  Error      :", err)
+	fmt.Println("  Status Code:", resp.StatusCode())
+	fmt.Println("  Status     :", resp.Status())
+	fmt.Println("  Proto      :", resp.Proto())
+	fmt.Println("  Time       :", resp.Time())
+	fmt.Println("  Received At:", resp.ReceivedAt())
+	fmt.Println("  Body       :\n", resp)
+	fmt.Println()
+
 	return nil
 }
 
+func (tm *remoteTaskManager) CreateVolume() (*models.Volume, error) {
+	volume := &models.Volume{}
+
+	resp, err := tm.client.R().
+		SetResult(volume).
+		Post("/volume")
+	if err != nil {
+		return nil, err
+	}
+
+	// Explore response object
+	fmt.Println("Response Info:")
+	fmt.Println("  ", resp.Request.URL)
+	fmt.Println("  Error      :", err)
+	fmt.Println("  Status Code:", resp.StatusCode())
+	fmt.Println("  Status     :", resp.Status())
+	fmt.Println("  Proto      :", resp.Proto())
+	fmt.Println("  Time       :", resp.Time())
+	fmt.Println("  Received At:", resp.ReceivedAt())
+	fmt.Println("  Body       :\n", resp)
+	fmt.Println()
+
+	return volume, nil
+}
+
+func (tm *remoteTaskManager) Login(username, password string) error {
+	request := map[string]interface{}{
+		"username": username,
+		"password": password,
+	}
+
+	resp, err := tm.client.R().
+		SetBody(request).
+		Post("/session/principal")
+	if err != nil {
+		return err
+	}
+
+	// Explore response object
+	fmt.Println("Response Info:")
+	fmt.Println("  ", resp.Request.URL)
+	fmt.Println("  Error      :", err)
+	fmt.Println("  Status Code:", resp.StatusCode())
+	fmt.Println("  Status     :", resp.Status())
+	fmt.Println("  Proto      :", resp.Proto())
+	fmt.Println("  Time       :", resp.Time())
+	fmt.Println("  Received At:", resp.ReceivedAt())
+	fmt.Println("  Body       :\n", resp)
+	fmt.Println()
+
+	return nil
+}
+
+func (tm *remoteTaskManager) Push(task *Task) (err error) {
+
+	if len(task.Outputs) == 1 {
+
+		_, err = tm.client.R().
+			EnableTrace().
+			SetBody(struct {
+				Token   string `json:"token"`
+				Outputs string `json:"outputs"`
+			}{
+				task.token,
+				string(task.Outputs[0]),
+			}).
+			Put(fmt.Sprintf("/task/%s", task.TaskId))
+
+	} else {
+
+		vol, err := tm.CreateVolume()
+		if err != nil {
+			return err
+		}
+
+		for index, output := range task.Outputs {
+			if len(output) == 0 {
+				continue
+			}
+			if err := tm.CreateFile(vol.Name, fmt.Sprintf("%d", index), output); err != nil {
+				return err
+			}
+		}
+
+		_, err = tm.client.R().
+			EnableTrace().
+			SetBody(struct {
+				Token   string `json:"token"`
+				Outputs string `json:"outputs"`
+			}{
+				task.token,
+				vol.Name,
+			}).
+			Put(fmt.Sprintf("/task/%s", task.TaskId))
+
+	}
+
+	return
+}
+
 func NewRemoteTaskManager(baseUrl string) TaskManager {
-	return &remoteTaskManager{
-		client:  resty.New(),
+	client := resty.New()
+	client.SetHostURL(fmt.Sprintf("%s/api/v1", baseUrl))
+	jar, _ := cookiejar.New(nil)
+	client.SetCookieJar(jar)
+
+	tm := &remoteTaskManager{
+		client:  client,
 		baseUrl: baseUrl,
 	}
+
+	err := tm.Login("judger1", "judger1")
+	if err != nil {
+		panic(err)
+	}
+
+	return tm
 }
